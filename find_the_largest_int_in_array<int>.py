dec4a3a0f349de0d16993578554885c5f17388e6
colney.dev <47986340+IsaacColney@users.noreply.github.com>
2020-03-30 14:25:26 +0530

    Update find_the_largest_int_in_array<int>.py

--- find_the_largest_int_in_array<int>.py
+++ find_the_largest_int_in_array<int>.py
@@ -8,16 +8,16 @@ if __name__ == '__main__':
     maps = map(int, input().split())
     arr = list(maps)
     print(arr)
+      li = 0
     for i in range(0,n-1):
-        li = 0
         if(i <= n - 1):
             if(arr[li]-arr[i+1] < 0):
                 largest = arr[i+1]
                 li = i+1
             elif(arr[li] - arr[i+1] == 0):
-                largest = arr[i+1]
-                li = i+1
+                largest = arr[li]
+                li = li
             elif(arr[li] - arr[i+1] > 0):
-                largest = arr[i]
-                li = i
+                largest = arr[li]
+                li = li
     print(largest)
